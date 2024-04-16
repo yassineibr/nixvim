fbd5d19cd648f9e2096354fe5c20d7ca6a8d142d
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-04-16 10:14:07 +0100

    add open mapping back

--- config/toggleterm.nix
+++ config/toggleterm.nix
@@ -1,6 +1,9 @@
 {
   plugins.toggleterm = {
     enable = true;
-    settings.direction = "horizontal";
+    settings = {
+      direction = "horizontal";
+      open_mapping = "'<C-t>'";
+    };
   };
 }
