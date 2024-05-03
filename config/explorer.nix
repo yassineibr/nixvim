ec4bb3865c1fb9a107dc2aefd7a6ac6e5af3447a
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-05-03 12:25:57 +0100

    add nvim-tree

--- config/explorer.nix
+++ config/explorer.nix
@@ -1,8 +1,17 @@
 {
+  plugins.nvim-tree.enable = true;
+
   keymaps = [
     {
       key = "<leader>e";
       action = "<cmd>Explore<CR>";
+      options = {
+        silent = true;
+      };
+    }
+    {
+      key = "<leader>t";
+      action = "<cmd>NvimTreeToggle<CR>";
       options = {
         silent = true;
       };
